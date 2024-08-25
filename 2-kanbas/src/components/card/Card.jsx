95283aff6e8607208b1c1ad0adfc08f6100bd480
jaimerod156 <jaimerodriguez156@gmail.com>
2024-08-25 15:36:27 -0400

    Incio proyecto 2-kanban con Vite, instalar react-router-don, instalar react-beautifuñ-dnd

--- 2-kanbas/src/components/card/Card.jsx
+++
@@ -1,9 +0,0 @@
-import './Card.css'
-
-export function Card(props){
-  return (
-    <div className="card">
-        {props.children}
-    </div>
-  );
-};
--- 2-kanbas/src/components/kanbas/Kanban.jsx
+++
@@ -1,5 +0,0 @@
-import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
-
-export function Kanban(){
-  return (<h1>Componente</h1>);
-};
