a4fcaab3235e47d04f2a42a8f623154a7948ffff
jaimerod156 <jaimerodriguez156@gmail.com>
2024-08-26 15:44:22 -0400

    Fin draggable, droppable, despliegue web

--- 2-kanban/src/main.jsx
+++ 2-kanban/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react' //no es compatible con react-beautiful-dnd
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <div>
     <App />
-  </StrictMode>,
+  </div>
 )
